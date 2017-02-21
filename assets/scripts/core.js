c692e3ea9e65c2abf529d3a1d2d988fbae574f70
jeanrobert <j_robert@mail.com>
2017-02-21 17:44:00 -0500

    Added contact form functionality.

--- assets/scripts/core.js
+++ assets/scripts/core.js
@@ -1,1 +1,24 @@
 /* core.js */
+
+///////////////////////////////////////////////////////
+// Function for contact form
+(function () {
+  'use strict';
+  
+  var name,
+      email,
+      phone,
+      subject,
+      message,
+      send = document.getElementById('user-send');
+  
+  send.onclick = function (e) {
+      name    = document.getElementById('user-name').value,
+      email   = document.getElementById('user-email').value,
+      subject = document.getElementById('user-subject').value,
+      message = document.getElementById('user-message').value,
+        
+      e.preventDefault();
+      window.location = 'mailto:chemserve@outlook.com?subject=' + subject + ' - ' + name + '&body=' + message;
+  };
+}());
