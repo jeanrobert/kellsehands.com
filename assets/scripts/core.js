34b593dd26be2412dcc6726513ca476a9a28e34c
Jean <j_robert@mail.com>
2017-02-22 00:12:50 -0500

    Fixed typos, changed footers, and edited contact form.

--- assets/scripts/core.js
+++ assets/scripts/core.js
@@ -1,6 +1,6 @@
 /* core.js */
 
-///////////////////////////////////////////////////////
+///////////////////////////////////
 // Function for contact form
 (function () {
   'use strict';
@@ -20,5 +20,29 @@
         
       e.preventDefault();
       window.location = 'mailto:chemserve@outlook.com?subject=' + subject + ' from ' + name + '&body=' + message;
+  };
+}());
+
+
+///////////////////////////////////
+// Function for mobile  menu
+(function () {
+  'use strict';
+  
+  var name,
+      email,
+      phone,
+      subject,
+      message,
+      send = document.getElementById('user-send');
+  
+  send.onclick = function (e) {
+      name    = document.getElementById('user-name').value,
+      email   = document.getElementById('user-email').value,
+      subject = document.getElementById('user-subject').value,
+      message = document.getElementById('user-message').value,
+        
+      e.preventDefault();
+      window.location = 'mailto:tom@kellsehand.com?subject=' + subject + ' from ' + name + '&body=' + message;
   };
 }());
